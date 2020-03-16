3548e9e99f589c8425220ef6ce7e74acb10641ce
Mai Hameed <maihameed@hotmail.com>
2020-03-16 13:58:55 -0400

    This is for u @rzgry

--- 1-change-bg-colour/scripts.js
+++ 1-change-bg-colour/scripts.js
@@ -1,5 +1,5 @@
 const colours = ["red", "orange", "yellow", "green", "blue", "purple"];
-var colour, newColour = document.getElementById("body").style.backgroundColor;
+let colour, newColour = document.querySelector("body").style.backgroundColor;
 
 function changeBackground(){
     // Ensure that the randomly picked new colour doesn't match current background colour
--- 2-change-bg-colour-hex/scripts.js
+++ 2-change-bg-colour-hex/scripts.js
@@ -1,4 +1,4 @@
-var colour, newColour = document.getElementById("body").style.backgroundColor;
+let colour, newColour = document.querySelector("body").style.backgroundColor;
 
 function changeBackground(){
     // Ensure that the randomly picked new colour doesn't match current background colour
@@ -6,7 +6,7 @@ function changeBackground(){
         // There are 16777216 different hex colour codes. Randomly pick one, then convert decimal to hex
         newColour = "#" + Math.floor(Math.random() * 16777217).toString(16).toUpperCase();
     }
-    document.body.style.backgroundColor = newColour;
+    document.querySelector("body").style.backgroundColor = newColour;
     document.getElementById("colourText").innerHTML = newColour;
     colour = newColour;
 }
